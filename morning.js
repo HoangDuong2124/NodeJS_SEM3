018f99508ae746dc9b69d4560804764cc553e3ea
HoangDuong2124 <103102495+HoangDuong2124@users.noreply.github.com>
2022-11-10 14:20:42 +0700

    node_bt
    node_bt

---
+++ morning.js
@@ -0,0 +1,12 @@
+var wakeUp = function() {
+    console.log("I'm waking up!");
+};
+var checkPhone = function() {
+    console.log("Checking phone...");
+};
+var eatBreakfast = function() {
+    console.log("I'm eating breakfast...");
+};
+wakeUp();
+checkPhone();
+eatBreakfast();
